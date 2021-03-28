b2d32b91cfc88ea23e3fa80afcff2acc65b895b8
Ahmed Moustafa <ahmed.moustafa@kp.org>
2021-03-28 20:28:40 +0200

    Initial Import

---
+++ R/colors.R
@@ -0,0 +1,55 @@
+#' evergreen_palettes palette
+#'
+evergreen_palettes <- list(
+  evergreen = c("#0e2217", "#8e3628", "#e4b48e", "#c2c9b9", "#8e5b1a", "#dde959")
+)
+
+
+#' evergreen palette generator
+#'
+#' @param n Number of colors desired.
+#'
+#' @return A vector of colors.
+#' @export
+#'
+#' @examples
+#' evergreen(3)
+
+evergreen <- function(n) {
+
+  pal <- evergreen_palettes[["evergreen"]]
+  if (is.null(pal))
+    stop("Palette not found.")
+
+  if (missing(n)) {
+    n <- length(pal)
+  }
+
+  if (n > length(pal)) {
+    stop("Number of requested colors greater than whatthis palette can offer")
+  }
+
+  out <- pal[1:n]
+
+  structure(out, class = "palette")
+}
+
+
+#' print.palette
+#' @export
+#' @importFrom graphics rect par image text
+#' @importFrom grDevices rgb
+#' @param x X
+#' @param ... Not sure
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n), col = x,
+        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, col = "#32373D")
+}
+
---
+++ README.md
@@ -0,0 +1,43 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# Evergreen
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+`evergreen` is an R package to provide a color palette based on the
+Evergreen ship which got stuck in Suez Canal in March 2021
+
+![Evergreen](img/evergreen.jpeg)
+
+<img src="man/figures/README-print.evergreen-1.png" width="100%" />
+
+## Installation
+
+You can install the development version of `evergreen` from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("ahmedmoustafa/evergreen")
+```
+
+## Example
+
+### A barplot with `evergreen`
+
+``` r
+library(evergreen)
+library(ggplot2)
+
+x = c("A", "B", "C", "D", "E", "F")
+y = c(60, 50, 40, 30, 5, 2.5)
+
+ggplot() +
+  geom_bar(aes(x = x, y = y, fill = x), stat = "identity") +
+  scale_fill_manual(values = evergreen()) +
+  ggtitle("Barplot with the Evergreen colors")
+```
+
+<img src="man/figures/README-evergreen.example-1.png" width="100%" />
